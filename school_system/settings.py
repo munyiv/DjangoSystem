8ae6c1a68b0bc015a19f432802d625eec6947c4a
student <audreymunyiva@gmail.com>
2021-10-05 11:29:12 +0300

    modified files

--- school_system/settings.py
+++ school_system/settings.py
@@ -81,12 +81,23 @@ WSGI_APPLICATION = 'school_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'schoolsystem',
+        'USER': 'audrey',
+        'PASSWORD': 'munyivaaudrey',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
